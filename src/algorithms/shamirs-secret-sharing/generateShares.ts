b16c2fe759d24f5c980302f9b880a5f5ffe8471a
Filip Sauer <sauer.filip@gmail.com>
2023-01-04 22:10:28 +0100

    simplify generateShares

--- src/algorithms/shamirs-secret-sharing/generateShares.ts
+++ src/algorithms/shamirs-secret-sharing/generateShares.ts
@@ -1,22 +1,16 @@
-import * as codec from "./codec";
-import { BIN_ENCODING, BYTE_PADDING, MAX_SHARES } from "./consts";
+import { BYTE_PADDING } from "./consts";
 import { points } from "./points";
-import { Buffer } from "buffer";
 import { Dispatch, SetStateAction } from "react";
 
-// n = MAX_SHARES
-// x = 0 ... n
-// y = n ... 2n
-
+// example
 // https://apogiatzis.medium.com/shamirs-secret-sharing-a-numeric-example-walkthrough-a59b288c34c4
+
 export const generateShares = (
   secret: string,
   shareCount: number,
   threshold: number,
   setGeneratingInfo: Dispatch<SetStateAction<Record<string, any>>>
 ): string[] => {
-  const scratch = new Array(2 * MAX_SHARES);
-
   const secretBuf = new TextEncoder().encode(secret);
   setGeneratingInfo((info) => ({
     ...info,
@@ -25,7 +19,8 @@ export const generateShares = (
   }));
 
   // append 1 to get extra padding, we'll account for this later
-  // divides binary string into 128-bit chunks (16 1-byte (8-bit) chunks) (array of 16 8-bit numbers (0-255))
+  // divides binary string into 128-bit chunks (array of 16 8-bit numbers (0-255)),
+  // pads with 0s if necessary to hide the length of the secret
   let parts = [...secretBuf, 1];
   for (
     let i = 0;
@@ -40,24 +35,13 @@ export const generateShares = (
     partsLen: parts.length,
   }));
   const pArr: { x: number; y: number }[][] = [];
+  const rawShares: string[][] = Array.from({ length: shareCount }, () => []);
   for (let i = 0; i < parts.length; ++i) {
     const p = points(parts[i], threshold, shareCount);
-    console.log(p);
     pArr.push(p);
 
-    for (let j = 0; j < shareCount; ++j) {
-      if (!scratch[j]) {
-        scratch[j] = p[j].x.toString(16);
-        // console.log("no scratch", p[j].x.toString(16));
-      }
-
-      const z = p[j].y.toString(2);
-      // console.log("y", p[j].y);
-      // console.log("z", z);
-      const y = scratch[j + MAX_SHARES] || "";
-      // console.log("y", y);
-      // y[j] = p[j][y] + y[j]
-      scratch[j + MAX_SHARES] = codec.pad(z) + y;
+    for (let j = 0; j < shareCount; j++) {
+      rawShares[j].push(p[j].y.toString(16).padStart(2, "0"));
     }
   }
   const pointsXY = pArr.map((part) =>
@@ -67,19 +51,17 @@ export const generateShares = (
     ...info,
     pointsXY,
     pointsXYLen: pointsXY.length,
-    // scratch: scratch,
   }));
 
-  for (let i = 0; i < shareCount; ++i) {
-    const x = scratch[i];
-    const y = codec.hex(scratch[i + MAX_SHARES], BIN_ENCODING);
-    console.log(x, y);
-    console.log([...scratch]);
-    scratch[i] = codec.encode(x, y);
-    console.log(codec.encode(x, y));
-    scratch[i] = Buffer.from("0" + scratch[i], "hex");
+  const result = [];
+  for (let i = 0; i < shareCount; i++) {
+    const str =
+      "08" +
+      (i + 1).toString(16).padStart(2, "0") +
+      rawShares[i].reverse().join("");
+
+    result.push(str);
   }
 
-  const result = scratch.slice(0, shareCount);
-  return result.map((r) => r.toString("hex"));
+  return result;
 };
